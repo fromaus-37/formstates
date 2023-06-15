eb7b820e1e1adfe293c994968f575ca749d7a991
fromaus <naveed_ausaf@hotmail.com>
2023-06-15 22:38:48 +0100

    Run tests in ci (Fix #3) (#7)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  //distDir: '.build/.next',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- public/city.js
+++ public/city.js
@@ -1,21 +1,21 @@
-const txtCity = document.querySelector("#txtCity");
-const btnSubmit = document.querySelector("#btnSubmit");
-const txtWrongAnswer = document.querySelector("#txtWrongAnswer");
-const txtLoading = document.querySelector("#txtLoading");
+const txtCity = document.querySelector('#txtCity');
+const btnSubmit = document.querySelector('#btnSubmit');
+const txtWrongAnswer = document.querySelector('#txtWrongAnswer');
+const txtLoading = document.querySelector('#txtLoading');
 
-const panelQuiz = document.querySelector("#panelQuiz");
-const panelResult = document.querySelector("#panelResult");
+const panelQuiz = document.querySelector('#panelQuiz');
+const panelResult = document.querySelector('#panelResult');
 
 const states = {
-  noError: "noError",
-  loading: "loading",
-  wrongAnswer: "wrongAnswer",
-  rightAnswer: "rightAnswer",
+  noError: 'noError',
+  loading: 'loading',
+  wrongAnswer: 'wrongAnswer',
+  rightAnswer: 'rightAnswer',
 };
 
 const submittableStates = {
-  submittable: "submittable",
-  notSubmittable: "notSubmittable",
+  submittable: 'submittable',
+  notSubmittable: 'notSubmittable',
 };
 
 function setSubmittableState(state) {
@@ -34,31 +34,31 @@ function setSubmittableState(state) {
 function setState(state) {
   switch (state) {
     case states.noError:
-      txtWrongAnswer.classList.add("invisible");
-      txtLoading.classList.add("invisible");
+      txtWrongAnswer.classList.add('invisible');
+      txtLoading.classList.add('invisible');
       txtCity.disabled = false;
-      panelQuiz.classList.remove("invisible");
-      panelResult.classList.add("invisible");
+      panelQuiz.classList.remove('invisible');
+      panelResult.classList.add('invisible');
       break;
     case states.loading:
-      txtWrongAnswer.classList.add("invisible");
-      txtLoading.classList.remove("invisible");
+      txtWrongAnswer.classList.add('invisible');
+      txtLoading.classList.remove('invisible');
       txtCity.disabled = true;
-      panelQuiz.classList.remove("invisible");
-      panelResult.classList.add("invisible");
+      panelQuiz.classList.remove('invisible');
+      panelResult.classList.add('invisible');
       resultTimeout = window.setTimeout(showResult, 5000);
       break;
     case states.wrongAnswer:
-      txtWrongAnswer.classList.remove("invisible");
-      txtLoading.classList.add("invisible");
+      txtWrongAnswer.classList.remove('invisible');
+      txtLoading.classList.add('invisible');
       txtCity.disabled = false;
-      panelQuiz.classList.remove("invisible");
-      panelResult.classList.add("invisible");
+      panelQuiz.classList.remove('invisible');
+      panelResult.classList.add('invisible');
       break;
     case states.rightAnswer:
-      panelQuiz.classList.add("invisible"); //now we don't care
+      panelQuiz.classList.add('invisible'); //now we don't care
       //    about state of other controls in this panel
-      panelResult.classList.remove("invisible");
+      panelResult.classList.remove('invisible');
       break;
     default:
       throw new Error(`UI state ${state} not understood`);
@@ -67,7 +67,7 @@ function setState(state) {
 
 function showResult() {
   window.clearTimeout(resultTimeout);
-  if (txtCity.value.trim().toLowerCase() === "istanbul") {
+  if (txtCity.value.trim().toLowerCase() === 'istanbul') {
     setState(states.rightAnswer);
   } else {
     setState(states.wrongAnswer);
@@ -90,5 +90,5 @@ function handleCityChange(e) {
   }
 }
 
-txtCity.addEventListener("input", handleCityChange);
-btnSubmit.addEventListener("click", handleSubmitClick);
+txtCity.addEventListener('input', handleCityChange);
+btnSubmit.addEventListener('click', handleSubmitClick);
