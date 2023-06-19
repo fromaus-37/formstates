c6761ef0baafcae1bd32de53d9f53b847e5dae37
fromaus-37 <naveed_ausaf@hotmail.com>
2023-06-19 10:26:20 +0100

    fix: configure path of page stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ const config: StorybookConfig = {
     '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
-    '../pages/**/*.stories.@(js|jsx|ts|tsx)',
+    '../pageStories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
